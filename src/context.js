778fabb71c5f5dfe5d7db3aea9c3cee18bbd1a6e
Floflow <floperrindb@gmail.com>
2021-05-13 09:54:43 +0100

    creation of the .env file to add the API Key to fetch the spoonacular API data. The .env file was added to gitignore

--- src/context.js
+++ src/context.js
@@ -6,12 +6,14 @@ const AppContext = React.createContext();
 const AppProvider = ({children}) => {
   const [loading, setLoading ] = useState(true);
   const [recipes, setRecipes ] = useState([]);
-  const [ search, setSearch ] = useState('a')
+  const [ search, setSearch ] = useState('pizza');
 
   const fetchRecipes = useCallback(async() =>{
     setLoading(true);
     try{
+      //I added "number="" params as I have limited requests per day. Remove it to get full result.
       const response = await fetch(`${url}${search}&number=1`);
+      console.log(`${url}${search}&number=1`)
       const data = await response.json()
       console.log(data)
     } catch (error) {
